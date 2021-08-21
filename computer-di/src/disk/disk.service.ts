c8f00c4d6feff653706325b909e150d47e39c28f
Alex <asasouza.alex@gmail.com>
2021-08-21 18:36:02 -0300

    Secao 6: Nest Modules + DI Container

---
+++ computer-di/src/disk/disk.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
+
+@Injectable()
+export class DiskService {
+    constructor(private readonly powerService: PowerService) {}
+
+    getData(): string {
+        console.log('Consuming 20 watts of power from PowerService');
+
+        this.powerService.supplyPower(20);
+
+        return 'data!';
+    }
+}
---
+++ computer-di/src/power/power.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PowerService {
+    supplyPower(watts: number): void {
+        console.log(`supplying ${watts} worth of power`);
+    }
+}
