636ba571db26c15ec26282a4962f494ac0df5bae
Alex <asasouza.alex@gmail.com>
2021-08-24 22:38:10 -0300

    Secao 9: Creating and Saving with repositories

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -7,7 +9,27 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   
   @Post('signup')
-  async createUser(@Body() body: CreateUserDTO): Promise<void> {
+  async createUser(@Body() body: CreateUserDTO): Promise<User> {
     return this.usersService.createUser(body);
+  }
+
+  @Get('/:id')
+  async findOne(@Param('id') userId: string): Promise<User> {
+    return this.usersService.findOne(parseInt(userId, 10));
+  }
+
+  @Get()
+  async findAllWithEmail(@Query('email') email: string): Promise<User[]> {
+    return this.usersService.findAllWithEmail(email);
+  }
+
+  @Delete('/:id')
+  async remove(@Param('id') userId: string): Promise<void> {
+    return this.usersService.remove(parseInt(userId, 10));
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') userId: string, @Body() body: UpdateUserDTO): Promise<User> {
+    return this.usersService.update(parseInt(userId, 10), body);
   }
 }
--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -1,14 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
 
-    // constructor(private readonly usersRepository: Repository<User>) {}
-    
-    async createUser(data: CreateUserDTO): Promise<void> {
-        console.log(data);
+  async createUser(data: CreateUserDTO): Promise<User> {
+    const { email, password } = data;
+    const newUser = this.usersRepository.create({ email, password });
+    // Chamar o metodo .save diretamente tambem funcionaria
+    // porem caso a entidade User aplicasse algum hook do TypeORM
+    // (AfterInsert, AfterUpdate, AfterRemove, etc) ele nao seria executado.
+    // Ao criar passar uma entidade para o .save, neste caso criada pelo metodo
+    // .create, os hooks seriam executadoss
+    return this.usersRepository.save(newUser);
+  }
+
+  async findOne(userId: number): Promise<User> {
+    const user = this.usersRepository.findOne(userId);
+    if(!user) {
+        // Lancar excecoes deste tipo dentro do Service pode nao ser
+        // apropriado, como esse tipo de excecao esta ligado diretamente
+        // ao protocolo HTTP caso a aplicacao tambem utiliza-se outro protocolo,
+        // como WebSocket, este erro nao seria correto e exigira refatoracao. 
+        // Uma solucao para garantir a reusabilidade do service é criar um 
+        // ExceptionFilter para ser utilizado em todos os services, onde as 
+        // camadas superiores da aplicacao lancassem o erro adequado.
+        throw new NotFoundException('user not found');
     }
+    return user;
+  }
+
+  async findAllWithEmail(email: string): Promise<User[]> {
+    return this.usersRepository.find({ email });
+  }
+
+  async update(userId: number, attrs: Partial<User>): Promise<User> {
+    const userToUpdate = await this.findOne(userId);
+
+    if (!userToUpdate) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(userToUpdate, attrs);
+    return this.usersRepository.save(userToUpdate);
+  }
+
+  async remove(userId: number): Promise<void> {
+    const userToRemove = await this.findOne(userId);
+    if (userToRemove) {
+        await this.usersRepository.remove(userToRemove);
+    }
+  }
 }
